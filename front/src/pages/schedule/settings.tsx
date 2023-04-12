a15018a8d495c01d8d55778792a4d9b1c15a86d9
栃澤侑人 <tochiman@MacBook-Pro-2.local>
2023-04-12 21:51:06 +0900

    [Update]dockerファイルの整備

--- front/src/pages/schedule/settings.tsx
+++ front/src/pages/schedule/settings.tsx
@@ -2,9 +2,23 @@ import Head from 'next/head'
 import Button from '@mui/material/Button'
 import { signOut, useSession} from 'next-auth/react'
 import Header from '../../component/Header'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import SupervisorAccountOutlinedIcon from '@mui/icons-material/SupervisorAccountOutlined';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
 
 export default function Settings() {
     const {data: session, status: loading} = useSession()
+
+    const drawerWidth = 200;
 
     if (!session){
         return null
@@ -20,6 +34,7 @@ export default function Settings() {
                     <link rel="icon" href="favicon.ico" />
                 </Head>
                 <Header site='settings'/>
+   
             </>
         )
     }
