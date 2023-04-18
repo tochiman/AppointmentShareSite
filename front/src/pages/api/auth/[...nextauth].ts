59b54af11140c8d14e5e917067835dbf1dc638f5
tochiman <developer@tochiman.com>
2023-04-19 07:29:09 +0900

    null

--- front/src/pages/api/auth/[...nextauth].ts
+++ front/src/pages/api/auth/[...nextauth].ts
@@ -2,19 +2,32 @@ import NextAuth from 'next-auth'
 import CredentialsProviders from "next-auth/providers/credentials"
 import GoogleProvider from 'next-auth/providers/google'
 
+type TypeResult = {
+  id: string;
+  username: string,
+  password: string,
+  email: string,
+  image: string,
+};
+
 // credentials の情報から、ログイン可能か判定してユーザー情報を返す関数
 const findUserByCredentials = credentials  => {
-  if (
-    credentials.email === process.env.USER_EMAIL && credentials.password === process.env.USER_PASSWORD
-  ) {
-    const secret = process.env.JWT_SECRET;
-    const token =  jwt.sign(credentials.email, secret); 
-    // ログイン可ならユーザー情報を返却
-    return { id: 1, name: "taro",  email: credentials.email, image: "https://img.icons8.com/pastel-glyph/64/000000/person-male--v2.png" , jwt: token}
-  } else {
-    // ログイン不可の場合は null を返却
-    return null
-  }
+  // let list: TypeResult[] = []
+  const url = process.env.API_URI + '/api/v1/user/info?email=' + credentials.email;
+  const result = fetch(url).then((result) => console.log(result.json()))
+  // if (result.status === '200 OK'){
+  //   list = result.content
+  //   console.log(list)
+  // } 
+  // if (
+  //   credentials.password === result.
+  // ) {
+  //   // ログイン可ならユーザー情報を返却
+  //   return { id: 1, name: "taro",  email: credentials.email, image: "https://img.icons8.com/pastel-glyph/64/000000/person-male--v2.png" }
+  // } else {
+  //   // ログイン不可の場合は null を返却
+  //   return null
+  // }
 }
 
 const options = {
@@ -31,10 +44,10 @@ const options = {
       async authorize(credentials) {
         const user = findUserByCredentials(credentials)
         if (user) {
-	  // 返されたオブジェクトはすべてJWTの`user`プロパティに保存される
+          // 返されたオブジェクトはすべてJWTの`user`プロパティに保存される
           return Promise.resolve(user)
         } else {
-	  // nullまたはfalseを返すと、認証を拒否する
+          // nullまたはfalseを返すと、認証を拒否する
           return Promise.resolve(null)
         }
       },
@@ -46,7 +59,7 @@ const options = {
     }),
   ],
   callbacks: {
-    async session({ session, token, user, account}) {
+    async session({ session, token }) {
       session.accessToken = token.accessToken;
       return session;
     },
